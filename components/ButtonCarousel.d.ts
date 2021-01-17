0a1a261e6d2df4c7928670f4a59f378f1b26b6dc
mshev <mshev@list.ru>
2021-01-17 19:50:21 +0300

    removed files

--- components/ButtonCarousel.d.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-export interface IButton {
-    handle: () => void;
-    customRender: React.ReactNode;
-    side?: 'left' | 'right' | undefined;
-}
-export declare const Button: ({ handle, customRender, side }: IButton) => JSX.Element;
--- components/Slide.d.ts
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-interface ISlide {
-    data: React.ReactNode;
-    id: number;
-    refSize: Object;
-    marginBlock: number;
-}
-export declare function Slide({ data, id, refSize, marginBlock, }: ISlide): JSX.Element;
-export {};
